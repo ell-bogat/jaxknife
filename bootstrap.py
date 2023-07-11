98092bc78d575f541af035e3979e446cbf7f0934
Ell Bogat <ell.bogat@nasa.gov>
2023-07-11 14:13:01 -0500

    separated array-based and looping bootstrap functions

--- bootstrap.py
+++ bootstrap.py
@@ -2,9 +2,9 @@ import jax
 import jax.numpy as jnp
 import numpy as np
 
-def bootstrap(data,estimator,n_resamples=1000,seed=0):
+def bootstrap_loop(data,estimator,n_resamples=1000,seed=0):
     """
-    Numpy bootstrap method
+    Bootstrap method using numpy and a for loop
 
     Input: 
     data        (array-like)
@@ -14,18 +14,41 @@ def bootstrap(data,estimator,n_resamples=1000,seed=0):
     """
 
     est_arr = np.zeros(n_resamples)
+    key = jax.random.PRNGKey(seed)
+
  
     for i in range(n_resamples):
         np.random.seed(seed)
-        resample = np.random.choice(data,(len(data),),replace=True,axis=0)
-        
+        key, subkey = jax.random.split(key)
+        resample = jax.random.choice(subkey,data,(len(data),),replace=True,axis=0)
         est_arr[i] = estimator(resample)
 
     return est_arr
 
+def bootstrap_nparray(data,estimator,n_resamples=1000,seed=0):
+    """
+    Bootstrap method using one large numpy array
+    ~~UNFINISHED~~
+
+    Input: 
+    data        (array-like)
+    estimator   (function)
+    n_samples   (int)
+    seed        (int)
+    """
+
+    est_arr = np.zeros(n_resamples)
+
+ 
+    np.random.seed(seed)
+    #resample = jax.random.choice(subkey,data,(len(data),),replace=True,axis=0)
+    #est_arr[i] = estimator(resample)
+
+    return 
+
 def jaxstrap(data,estimator_func,n_resamples=1000,seed=0):
     """
-    JAX accelerated bootstrap method
+    Bootstrap method using JAX arrays
     Input: 
     data        (array-like)
     estimator   (function)
