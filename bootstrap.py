b1ab3b374ec71b77df6ec1edd54321dae072b171
Ell Bogat <ell.bogat@nasa.gov>
2023-07-11 15:40:19 -0500

    changed estimator_func variable name to statistic_func

--- bootstrap.py
+++ bootstrap.py
@@ -2,15 +2,15 @@ import jax
 import jax.numpy as jnp
 import numpy as np
 
-def bootstrap(data,estimator_func,n_resamples=1000,seed=0):
+def bootstrap(data,statistic_func,n_resamples=1000,seed=0):
     """
     Bootstrap method using numpy and a for loop
 
     Input: 
-    data        (array-like)
-    estimator_func   (function)
-    n_samples   (int)
-    seed        (int)
+    data            (array-like)
+    statistic_func  (function)
+    n_samples       (int)
+    seed            (int)
     """
 
     est_arr = np.zeros(n_resamples)
@@ -20,27 +20,25 @@ def bootstrap(data,estimator_func,n_resamples=1000,seed=0):
         idx = np.random.randint(len(data), size=len(data))
         resample = data[idx]
         
-        est_arr[i] = estimator_func(resample)
+        est_arr[i] = statistic_func(resample)
 
     return est_arr
 
-def jaxstrap(data,estimator_func,n_resamples=1000,seed=0):
+def jaxstrap(data,statistic_func,n_resamples=1000,seed=0):
     """
     Bootstrap method using JAX arrays
     Input: 
-    data        (array-like)
-    estimator   (function)
-    n_samples   (int)
-    seed        (int)
+    data            (array-like)
+    statistic_func  (function)
+    n_samples       (int)
+    seed            (int)
     """
-
-    # TO DO: change this to one giant array so we can do it in JAX -Ell
 
     key = jax.random.PRNGKey(seed)
 
     resamples = jax.random.choice(key,data,(n_resamples,*data.shape),replace=True)
         
-    estimator_vectorized = jax.vmap(estimator_func)
-    est_arr = estimator_vectorized(resamples)
+    statistic_vectorized = jax.vmap(statistic_func)
+    est_arr = statistic_vectorized(resamples)
 
     return est_arr
