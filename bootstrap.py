ecd6ba53affb25b0712758b28d9dbf99dcabfc17
Ell Bogat <ell.bogat@nasa.gov>
2023-07-11 08:42:02 -0500

    separated numpy and jax versions of bootstrap, untested

--- bootstrap.py
+++ bootstrap.py
@@ -11,16 +11,34 @@ def bootstrap(data,estimator,n_resamples=1000,seed=0):
     seed        (int)
     """
 
+    est_arr = np.zeros(n_resamples)
+ 
+    for i in range(n_resamples):
+        np.random.seed(seed)
+        resample = np.random.choice(data,(len(data),),replace=True,axis=0)
+        
+        est_arr[i] = estimator(resample)
+
+    return est_arr
+
+def jaxstrap(data,estimator,n_resamples=1000,seed=0):
+    """
+    Input: 
+    data        (array-like)
+    estimator   (function)
+    n_samples   (int)
+    seed        (int)
+    """
+
     # TO DO: change this to one giant array so we can do it in JAX -Ell
 
     key = jax.random.PRNGKey(seed)
 
     est_arr = np.zeros(n_resamples)
  
-    for i in range(n_resamples):
-        key, subkey = jax.random.split(key)
-        resample = jax.random.choice(subkey,data,(len(data),),replace=True,axis=0)
+    key, subkey = jax.random.split(key)
+    resamples = jax.random.choice(subkey,data,(n_resamples,*data.shape),replace=True)
         
-        est_arr[i] = estimator(resample)
+    est_arr[i] = estimator(resamples)
 
     return est_arr
