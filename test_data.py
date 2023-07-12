e989c33758256b1b6616e29bb99e7af0428c48dc
Ell Bogat <ell.bogat@nasa.gov>
2023-07-12 09:21:38 -0500

    added position-velocity test data structure

--- test_data.py
+++ test_data.py
@@ -1,9 +1,14 @@
 import numpy as np
 
-seed = 0
-np.random.seed(seed)
-random_positions = np.random.uniform(-1, 1, size=(100, 3))
+def make_pos_data():
+    seed = 0
+    np.random.seed(seed)
+    return np.random.uniform(-1, 1, size=(100, 3))
 
+def make_posvel_data():
+    seed = 0
+    np.random.seed(seed)
+    return np.random.uniform(-1, 1, size=(100, 2, 3))
 
 if __name__=='__main__':
     pass
