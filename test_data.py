997c8358c56481291a1ac4fee1105ce1f4c2a6fe
Ell Bogat <ell.bogat@nasa.gov>
2023-07-12 09:45:25 -0500

    converted to callable functions, added 3D posvel data

--- test_data.py
+++ test_data.py
@@ -1,12 +1,10 @@
 import numpy as np
 
-def make_pos_data():
-    seed = 0
+def make_pos_data(seed=0):
     np.random.seed(seed)
     return np.random.uniform(-1, 1, size=(100, 3))
 
-def make_posvel_data():
-    seed = 0
+def make_posvel_data(seed=0):
     np.random.seed(seed)
     return np.random.uniform(-1, 1, size=(100, 2, 3))
 
